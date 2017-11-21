569ac08e86cabdcd3f0f851f36abffaf8f100fde
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-21 15:04:33 +0200

    better code

--- config/python.py
+++ config/python.py
@@ -1,10 +1,10 @@
 entry_points = {
     'console_scripts': [
-	'pypitools_install_from_local=pypitools.scripts.install_from_local:main',
-	'pypitools_install_from_remote=pypitools.scripts.install_from_remote:main',
-	'pypitools_register=pypitools.scripts.register:main',
-	'pypitools_upload=pypitools.scripts.upload:main',
-	'pypitools_check_config=pypitools.scripts.check_config:main',
+        'pypitools_install_from_local=pypitools.scripts.install_from_local:main',
+        'pypitools_install_from_remote=pypitools.scripts.install_from_remote:main',
+        'pypitools_register=pypitools.scripts.register:main',
+        'pypitools_upload=pypitools.scripts.upload:main',
+        'pypitools_check_config=pypitools.scripts.check_config:main',
     ],
 }
 install_requires = [
@@ -15,6 +15,6 @@ install_requires = [
     'pypitools',  # for upload etc
     'pydmt',  # for building
     'Sphinx',  # for the sphinx builder
-    ]
-requirements3 = install_requires 
+]
+requirements3 = install_requires
 python_requires = ">=3"
