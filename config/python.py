2f6ef5b8b7a460a85d2e5eb1965717d0564be079
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-30 12:49:56 +0200

    bug fix

--- config/python.py
+++ config/python.py
@@ -1,12 +1,10 @@
-entry_points = {
-    'console_scripts': [
-        'pypitools_install_from_local=pypitools.scripts.install_from_local:main',
-        'pypitools_install_from_remote=pypitools.scripts.install_from_remote:main',
-        'pypitools_register=pypitools.scripts.register:main',
-        'pypitools_upload=pypitools.scripts.upload:main',
-        'pypitools_check_config=pypitools.scripts.check_config:main',
-    ],
-}
+console_scripts = [
+    'pypitools_install_from_local=pypitools.scripts.install_from_local:main',
+    'pypitools_install_from_remote=pypitools.scripts.install_from_remote:main',
+    'pypitools_register=pypitools.scripts.register:main',
+    'pypitools_upload=pypitools.scripts.upload:main',
+    'pypitools_check_config=pypitools.scripts.check_config:main',
+]
 setup_requires = [
 ]
 run_requires = [
