189332c27aa4de68eef402762dcac18bc73c36a9
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-02 22:52:24 +0200

    new template

--- config/python.py
+++ config/python.py
@@ -1,3 +1,7 @@
+import config.project
+
+package_name = config.project.project_name
+
 console_scripts = [
     'pypitools_install_from_local=pypitools.scripts.install_from_local:main',
     'pypitools_install_from_remote=pypitools.scripts.install_from_remote:main',
@@ -5,18 +9,23 @@ console_scripts = [
     'pypitools_upload=pypitools.scripts.upload:main',
     'pypitools_check_config=pypitools.scripts.check_config:main',
 ]
+
 setup_requires = [
 ]
+
 run_requires = [
     'click',  # for command line parsing
     'pylogconf',  # for configuring logging
 ]
+
 dev_requires = [
     'pyclassifiers',  # for software classification
     'pypitools',  # for upload etc
     'pydmt',  # for building
     'Sphinx',  # for the sphinx builder
 ]
+
 install_requires = list(setup_requires)
 install_requires.extend(run_requires)
+
 python_requires = ">=3"
