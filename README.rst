e7dc50abc482e777f940e295379ec4d8f84af5d5
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-31 05:51:57 +0300

    better code

--- README.rst
+++ README.rst
@@ -2,4 +2,4 @@
 *pypitools* project by Mark Veltzer
 =======================
 
-version: (0, 0, 50)
+version: (0, 0, 51)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pypitools',
-    version='0.0.50',
+    version='0.0.51',
     packages=[
         'pypitools',
         'pypitools.endpoints',
