7d0b6fdb29cf9d16410cd5b8768cda10b14050f9
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-05 19:36:26 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pypitools',
-    version='0.0.51',
+    version='0.0.52',
     packages=[
         'pypitools',
         'pypitools.endpoints',
