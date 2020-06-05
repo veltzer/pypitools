2c5f6923d436a8483dd04e051e70780f5963cac4
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-05 19:41:37 +0300

    added check command

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pypitools',
-    version='0.0.52',
+    version='0.0.53',
     packages=[
         'pypitools',
         'pypitools.endpoints',
