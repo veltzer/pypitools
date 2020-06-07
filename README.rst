664a0fb7ac3117f6d334f5628007d97bf7aac5ea
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-07 13:51:21 +0300

    trying better rst and long desc

--- README.rst
+++ README.rst
@@ -2,9 +2,9 @@
 *pypitools*
 ===========
 
-.. image:: https://img.shields.io/github/license/veltzer/pydmt   :alt: GitHub
-
-![build](https://github.com/veltzer/pypitools/workflows/build/badge.svg)
+.. image:: https://img.shields.io/pypi/v/pypitools :alt: PyPI
+.. image:: https://img.shields.io/github/license/veltzer/pypitools :alt: GitHub
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg :alt: Code Style: black
 
 project website: https://veltzer.github.io/pypitools
 
--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 import setuptools
 
+
+def get_readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pypitools",
@@ -10,7 +16,7 @@ setuptools.setup(
     ],
     # from here all is optional
     description="pypitools helps you with various pypi tasks",
-    long_description="pypitools is a collection of utilities to help interact with the pypi repository",
+    long_description=get_readme(),
     long_description_content_type="text/x-rst",
     author="Mark Veltzer",
     author_email="mark.veltzer@gmail.com",
