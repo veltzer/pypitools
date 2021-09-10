c0c85f16bfafa7d9c7686094f09feeecd2ea4bde
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-10 19:48:07 +0300

    fix config

--- config/project.py
+++ config/project.py
@@ -57,13 +57,13 @@ else:
     project_copyright_years_short = f"{project_year_started}-{config.general.general_current_folder}"
 
 project_google_analytics_snipplet = f"""<script type="text/javascript">
-(function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
+(function(i,s,o,g,r,a,m){{i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){{
 (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-}})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+}})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
 
-ga('create', '{project_google_analytics_tracking_id}', 'auto');
-ga('send', 'pageview');
+ga("create", "{project_google_analytics_tracking_id}", "auto");
+ga("send", "pageview");
 
 </script>"""
 project_paypal_donate_button_snipplet = f"""<form action="https://www.paypal.com/cgi-bin/webscr"
