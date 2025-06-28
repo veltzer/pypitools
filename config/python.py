60bf1f191d64b3c22945d89e7dbabb1b5d7ffcb7
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:21 +0300

    more

--- config/python.py
+++ config/python.py
@@ -1,8 +1,8 @@
 """ python deps for this project """
 
-console_scripts: list[str] = [
-    "pypitools=pypitools.main:main",
-]
+scripts: dict[str,str] = {
+    "pypitools": "pypitools.main:main",
+}
 
 config_requires: list[str] = [
     "pyclassifiers",
