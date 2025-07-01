7ffd9730ef0b79e42141e511caeb5bff8b2db74a
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:16 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,12 +1,7 @@
 """ python deps for this project """
 
-scripts: dict[str,str] = {
-    "pypitools": "pypitools.main:main",
-}
+import config.shared
 
-config_requires: list[str] = [
-    "pyclassifiers",
-]
 install_requires: list[str] = [
     "pytconf",
     "pylogconf",
@@ -14,16 +9,10 @@ install_requires: list[str] = [
     "wheel",
     "setuptools",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-]
-requires = config_requires + install_requires + build_requires + test_requires
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+requires = install_requires + build_requires + test_requires
+
+scripts: dict[str,str] = {
+    "pypitools": "pypitools.main:main",
+}
